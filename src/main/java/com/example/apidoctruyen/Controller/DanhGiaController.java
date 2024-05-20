3bf9ed36c16f972a150af65f405f66858d50f0a8
QuocThinhNguyen <nguyenquocthinh111@gmail.com>
2024-05-20 11:49:37 +0700

    put

--- src/main/java/com/example/apidoctruyen/Controller/DanhGiaController.java
+++ src/main/java/com/example/apidoctruyen/Controller/DanhGiaController.java
@@ -1,11 +1,13 @@
 package com.example.apidoctruyen.Controller;
 
+import com.example.apidoctruyen.entity.Danhgia;
 import com.example.apidoctruyen.model.TruyenDto;
 import com.example.apidoctruyen.repository.BinhLuanRepository;
 import com.example.apidoctruyen.repository.DanhGiaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,5 +20,19 @@ public class DanhGiaController {
     public Double getTruyenById(@PathVariable int id) {
         Double danhgia = repo.getAverageRatingByTruyenId(id);
         return danhgia;
+    }
+    // cap nhat so sao theo idchapter va idtaikhoan trong bảng danhgia
+//    @PutMapping ("/danhgia/{idchapter}/{idtaikhoan}/{sosao}")
+//    public void updateDanhGia(@PathVariable Integer idchapter, @PathVariable Integer idtaikhoan, @PathVariable Integer sosao){
+//        repo.updateDanhGia(idchapter, idtaikhoan, sosao);
+//    }
+
+    @PutMapping ("/danhgia/{idchapter}/{idtaikhoan}/{sosao}")
+    public void updateDanhGia(@PathVariable Integer idchapter, @PathVariable Integer idtaikhoan, @PathVariable Double sosao){
+        Danhgia danhgia = repo.findByIdchapterAndIdtaikhoan(idchapter, idtaikhoan);
+        if (danhgia != null) {
+            danhgia.setSosao(sosao);
+            repo.save(danhgia);
+        }
     }
 }
--- src/main/java/com/example/apidoctruyen/repository/DanhGiaRepository.java
+++ src/main/java/com/example/apidoctruyen/repository/DanhGiaRepository.java
@@ -11,4 +11,12 @@ import java.util.List;
 public interface DanhGiaRepository extends JpaRepository<Danhgia, Integer> {
     @Query("SELECT round(avg(d.sosao), 2) FROM Danhgia d WHERE d.idchapter.id IN (SELECT c.id FROM Chapter c WHERE c.idtruyen.id = :idtruyen)")
     Double getAverageRatingByTruyenId(int idtruyen);
+
+    // updateDanhGia
+    @Query("UPDATE Danhgia d SET d.sosao = :sosao WHERE d.idchapter.id = :idchapter AND d.idtaikhoan.id = :idtaikhoan")
+    void updateDanhGia(Integer idchapter, Integer idtaikhoan, Integer sosao);
+
+    @Query("SELECT d FROM Danhgia d WHERE d.idchapter.id = :idchapter AND d.idtaikhoan.id = :idtaikhoan")
+    Danhgia findByIdchapterAndIdtaikhoan(Integer idchapter, Integer idtaikhoan);
+
 }
